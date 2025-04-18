c13d3dc7b6a84a711abdae933670aa16ebfc8422
Livia Coadă <120571940+LiviaCd@users.noreply.github.com>
2025-04-18 15:47:21 +0300

    Init

---
+++ Anagram/anagram_finder.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+import time
+from pathlib import Path
+
+basePath = 'D:\\Github Projects\\AnagramFinder\\Anagram\\'
+
+def main():
+
+    start_time = time.time()
+
+    contents = Path(basePath + 'sample.txt').read_text()
+    #contents = Path(basePath + '10_million_real_words.txt').read_text()
+
+    words = contents.split()
+
+    anagram_groups = defaultdict(list)
+    for word in words:
+        sorted_letters = ''.join(sorted(word))
+        key = sorted_letters
+        anagram_groups[key].append(word)
+
+    output_path = basePath + 'anagram_groups.txt'
+    #output_path = basePath + 'anagram_groups_10_million.txt'
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        for group in anagram_groups.values():
+            f.write(' '.join(group) + '\n')
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print("The results have been successfully saved")
+
+    print(f"The program ran in {execution_time:.6f} seconds")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
